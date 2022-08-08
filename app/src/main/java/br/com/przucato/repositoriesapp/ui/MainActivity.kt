cea063af168edf2a1e771adc375a48518869b19f
przucato <pri.zucato@hotmail.com>
2022-08-08 18:33:11 -0300

    Base project

--- app/src/main/java/br/com/przucato/repositoriesapp/ui/MainActivity.kt
+++ app/src/main/java/br/com/przucato/repositoriesapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package br.com.przucato.repositoriesapp
+package br.com.przucato.repositoriesapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.przucato.repositoriesapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
